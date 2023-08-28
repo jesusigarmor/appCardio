56e22f97bf1938d954c0ddd6d6684d91d1880499
Jesus Garcia <jesus.igarmor@gmail.com>
2023-08-27 22:36:12 -0600

    Configuracion inicial de app
    Configuracion inicial de la app, configuracion de splash screen

---
+++ android/app/src/main/kotlin/com/example/appcardio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appcardio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme:
+          ThemeData(brightness: Brightness.light, primarySwatch: Colors.indigo),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.system,
+      home: AppHome(),
+    );
+  }
+}
+
+class AppHome extends StatelessWidget {
+  const AppHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Cardio AI")),
+      body: const Center(child: Text("Login")),
+    );
+  }
+}
